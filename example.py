75a209756d02b47c19e148f0f8547222472d9f47
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2023-12-11 23:50:17 -0300

    Added energy balance mode to GasTurbine

--- example.py
+++ example.py
@@ -19,53 +19,66 @@ gas_initial_state = Fluid(FluidsList.Air).with_state(
 
 # Creating Brayton cycle instance:
 brayton_cycle = BraytonCycle(initial_state=gas_initial_state, mass_flux=450)
-brayton_cycle.add_device(
-    GasCompressor(
-        name="C1g",
-        efficiency=0.8,
-        compression_ratio=25,
-    )
+
+# Defining devices:
+compressor_1g = GasCompressor(
+    name="C1g",
+    efficiency=0.8,
+    compression_ratio=25,
 )
-brayton_cycle.add_device(
-    GasCombustionChamber(
-        name="CC1g",
-        outlet_temperature=1000,
-    )
-)
-brayton_cycle.add_device(
-    GasTurbine(
-        name="TCg",
-        efficiency=0.8,
-        outlet_pressure=bar_to_pascal(5),
-    )
-)
-brayton_cycle.add_device(
-    GasCombustionChamber(
-        name="CC2g",
-        outlet_temperature=710,
-    )
-)
-brayton_cycle.add_device(
-    GasTurbine(
-        name="TPg",
-        efficiency=0.85,
-        outlet_pressure=101325,
-    )
+combustion_chamber_1g = GasCombustionChamber(
+    name="CC1g",
+    outlet_temperature=1000,
 )
 
-brayton_cycle.solve()
+state_1g = brayton_cycle.initial_state
+state_2g = compressor_1g.get_outlet_state(state_1g)
+state_3g = combustion_chamber_1g.get_outlet_state(state_2g)
+
+turbine_1g = GasTurbine(
+    name="TCg",
+    efficiency=0.8,
+    outlet_pressure=bar_to_pascal(8),
+    energy_balance=state_2g.enthalpy - state_1g.enthalpy,
+)
+combustion_chamber_2g = GasCombustionChamber(
+    name="CC2g",
+    outlet_temperature=710,
+)
+turbine_2g = GasTurbine(
+    name="TPg",
+    efficiency=0.85,
+    outlet_pressure=bar_to_pascal(1),
+)
+
+state_4g = turbine_1g.get_outlet_state(state_3g)
+state_5g = combustion_chamber_2g.get_outlet_state(state_4g)
+state_6g = turbine_2g.get_outlet_state(state_5g)
+
+devices = [
+    compressor_1g,
+    combustion_chamber_1g,
+    turbine_1g,
+    combustion_chamber_2g,
+    turbine_2g,
+]
+
+states = [state_1g, state_2g, state_3g, state_4g, state_5g, state_6g]
+
+brayton_cycle.devices = devices
+brayton_cycle.states = states
 
 # Results:
 print("BRAYTON GAS CYCLE\n")
 
-for i, state in enumerate(brayton_cycle.states):
+for i, state in enumerate(states):
     print(
         f"{i+1} - {state.name}: {state.pressure * 1e-5:.2f} bar, {state.temperature:.2f} C, {state.enthalpy * 1e-3:.2f} kJ/kg"
     )
 
 print("\n")
 
-for device in brayton_cycle.devices:
+for device in devices:
     print(
         f"{device.name}: {device.energy_balance * 1e-3:.2f} kJ/kg",
     )
--- thermosys/models/devices.py
+++ thermosys/models/devices.py
@@ -27,12 +27,13 @@ class Device(ABC):
     get_outlet_state: Returns the state of the gas at the outlet of the device.
     """
 
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, energy_balance: float | None = None) -> None:
         """
         Initializes a new instance of the Device class.
 
         Parameters:
         name (str): The name or identifier of the device.
+
         """
         self.name = name
 
@@ -59,6 +60,7 @@ class Device(ABC):
         Returns:
         Fluid: The state of the gas at the outlet of the device.
         """
+        self.inlet_state = inlet_state
 
     @property
     def energy_balance(self) -> float:
@@ -109,10 +111,7 @@ class GasCompressor(NonIsentropicDevice):
     """
 
     def __init__(
-        self,
-        name: str,
-        efficiency: float,
-        compression_ratio: float,
+        self, name: str, efficiency: float, compression_ratio: float
     ) -> None:
         """
         Initializes a new instance of the GasCompressor class.
@@ -120,7 +119,7 @@ class GasCompressor(NonIsentropicDevice):
         Parameters:
         name (str): The name or identifier of the compressor.
         efficiency (float): The efficiency of the compressor (as a decimal).
-        compression_ratio (float): The ratio of the outlet pressure to the inlet pressure.
+        compression_ratio (float | None): The ratio of the outlet pressure to the inlet pressure.
         """
         super().__init__(name, efficiency)
         self.compression_ratio = compression_ratio
@@ -130,6 +129,8 @@ class GasCompressor(NonIsentropicDevice):
         return "compressor"
 
     def get_outlet_state(self, inlet_state: Fluid) -> Fluid:
+        super().get_outlet_state(inlet_state)
+
         inlet_pressure = inlet_state.pressure
         inlet_temperature = inlet_state.temperature
 
@@ -163,6 +164,7 @@ class GasCombustionChamber(Device):
         self,
         name: str,
         outlet_temperature: float,
+        energy_balance: float | None = None,
     ) -> None:
         """
         Initializes a new instance of the GasCombustionChamber class.
@@ -172,7 +174,7 @@ class GasCombustionChamber(Device):
         outlet_temperature (float): The temperature of the gas at the outlet
             of the combustion chamber (in C).
         """
-        super().__init__(name)
+        super().__init__(name, energy_balance=energy_balance)
         self.outlet_temperature = outlet_temperature
 
     @property
@@ -180,6 +182,8 @@ class GasCombustionChamber(Device):
         return "combustion_chamber"
 
     def get_outlet_state(self, inlet_state: Fluid) -> Fluid:
+        super().get_outlet_state(inlet_state)
+
         pressure = inlet_state.pressure
 
         fluid = Fluid(FluidsList.Air).with_state(
@@ -197,6 +201,7 @@ class GasTurbine(NonIsentropicDevice):
 
     Attributes:
     outlet_pressure (float): The pressure of the gas at the outlet of the turbine (in Pascals).
+    _energy_balance (float | None): The energy balance of the turbine (J/kg).
 
     Methods:
     __init__: Initializes a new instance of the GasTurbine class.
@@ -207,6 +212,7 @@ class GasTurbine(NonIsentropicDevice):
         name: str,
         efficiency: float,
         outlet_pressure: float,
+        energy_balance: float | None = None,
     ) -> None:
         """
         Initializes a new instance of the GasTurbine class.
@@ -215,25 +221,40 @@ class GasTurbine(NonIsentropicDevice):
         name (str): The name or identifier of the gas turbine.
         efficiency (float): The efficiency of the turbine (as a decimal).
         outlet_pressure (float): The pressure of the gas at the outlet of the turbine (in Pascals).
+        energy_balance (float | None): The energy balance of the turbine (J/kg).
         """
         super().__init__(name, efficiency)
         self.outlet_pressure = outlet_pressure
+        self._energy_balance = energy_balance
 
     @property
     def device_type(self) -> str:
         return "turbine"
 
     def get_outlet_state(self, inlet_state: Fluid) -> Fluid:
-        inlet_pressure = inlet_state.pressure
-        inlet_temperature = inlet_state.temperature
+        super().get_outlet_state(inlet_state)
 
-        fluid = Fluid(FluidsList.Air).with_state(
-            Input.pressure(inlet_pressure),
-            Input.temperature(inlet_temperature),
-        )
+        if self._energy_balance is not None:
+            outlet_enthalpy = (
+                inlet_state.enthalpy - self._energy_balance
+            ) / self.efficiency
+            pressure_drop = np.abs(inlet_state.pressure - self.outlet_pressure)
 
-        self.outlet_state = fluid.expansion_to_pressure(
-            pressure=self.outlet_pressure,
-            isentropic_efficiency=self.efficiency * 100,
-        )
+            self.outlet_state = inlet_state.cooling_to_enthalpy(
+                enthalpy=outlet_enthalpy, pressure_drop=pressure_drop
+            )
+        else:
+            inlet_pressure = inlet_state.pressure
+            inlet_temperature = inlet_state.temperature
+
+            fluid = Fluid(FluidsList.Air).with_state(
+                Input.pressure(inlet_pressure),
+                Input.temperature(inlet_temperature),
+            )
+
+            self.outlet_state = fluid.expansion_to_pressure(
+                pressure=self.outlet_pressure,
+                isentropic_efficiency=self.efficiency * 100,
+            )
+
         return self.outlet_state
