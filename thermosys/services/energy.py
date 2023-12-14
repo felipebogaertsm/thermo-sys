c894b376b06ecad0e4cc5d746f2fccb32a8569a4
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2023-12-14 15:05:00 -0300

    ipynb example

--- thermosys/services/energy.py
+++ thermosys/services/energy.py
@@ -2,6 +2,7 @@
 Energy calculation functions.
 """
 
+import numpy as np
 from pyfluids import Fluid
 
 
@@ -9,4 +10,4 @@ def energy_balance(state_1: Fluid, state_2: Fluid) -> float:
     """
     Calculates the energy balance between two states.
     """
-    return state_2.enthalpy - state_1.enthalpy
+    return np.abs(state_2.enthalpy - state_1.enthalpy)
