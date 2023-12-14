bab8bf1e66349f471113d5f3a658aacc612b8a12
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2023-12-14 18:20:29 -0300

    Removed plot and completed example

--- thermosys/services/montecarlo.py
+++ thermosys/services/montecarlo.py
@@ -1,5 +1,4 @@
 import numpy as np
-import plotly.graph_objects as go
 
 
 def get_random_value(min_value: float, max_value: float) -> float:
@@ -14,26 +13,3 @@ def get_random_value(min_value: float, max_value: float) -> float:
     float: A random value within the specified range.
     """
     return np.random.uniform(min_value, max_value)
-
-
-import plotly.express as px
-
-
-def plot_monte_carlo(array_1: np.ndarray, array_2: np.ndarray):
-    """
-    Plot a Monte Carlo simulation result showing the relationship between pressure and efficiency using Plotly.
-
-    Parameters:
-    array_1 (np.ndarray): Array of pressure values.
-    array_2 (np.ndarray): Array of efficiency values corresponding to the array_1.
-    """
-    if array_1.shape != array_2.shape:
-        raise ValueError("The shapes of array_1 and array_2 must be the same.")
-
-    fig = go.Figure(data=go.Scatter(x=array_1, y=array_2, mode="markers"))
-    fig.update_layout(
-        title="Monte Carlo Simulation: Pressure vs Efficiency",
-        xaxis_title="Pressure",
-        yaxis_title="Efficiency",
-    )
-    fig.show()
